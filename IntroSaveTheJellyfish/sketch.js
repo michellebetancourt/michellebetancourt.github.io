483870e0833c2a63a21cf2e0239e32599b2f72fc
michellebetancourt <mnbetancourt@udlanet.ec>
2016-04-13 00:06:34 -0500

    con_sprite

--- IntroSaveTheJellyfish/sketch.js
+++ IntroSaveTheJellyfish/sketch.js
@@ -3,6 +3,7 @@
 //animations like p5 images should be stored in variables
 //in order to be displayed during the draw cycle
 var med, tib;
+var medani, tibani;
 var fondo, howto, gamename, star;
 //it's advisable (but not necessary) to load the images in the preload function
 //of your sketch otherwise they may appear with a little delay
@@ -10,8 +11,8 @@ function preload() {
 
   //create an animation from a sequence of numbered images
   //pass the first and the last file name and it will try to find the ones in between
-  med = loadAnimation("assets/1.png", "assets/52.png");
-  tib = loadAnimation("assets/mt1.png", "assets/mt16.png");
+  medani = loadAnimation("assets/1.png", "assets/52.png");
+  tibani = loadAnimation("assets/mt1.png", "assets/mt16.png");
   fondo = loadImage("assets/FondoJuegoIntro.jpg");
   howto = loadImage("assets/How to play.png");
   gamename = loadImage("assets/GameName.png");
@@ -22,15 +23,22 @@ function preload() {
 
 function setup() {
   // createCanvas(1280,755);
-  createCanvas(1280, 755);
+  // createCanvas(displayWidth, displayHeight);
+  createCanvas(windowWidth, windowHeight);
+med= createSprite ( width * 0.85, height * 0.70,width * 0.20, height * 0.30);
+med.addAnimation('flotar',medani);
+tib= createSprite (width * 0.25, height * 0.65,width * 0.30, height * 0.20);
+tib.addAnimation('flotart',tibani);
 }
 
 function draw() {
-  background(fondo, displayWidth, displayHeight);
+  background(fondo,windowWidth, windowHeight);
   //specify the animation instance and its x,y position
   //animation() will update the animation frame as well
-  animation(med, width * 0.85, height * 0.70);
-  animation(tib, width * 0.25, height * 0.65);
+  
+  drawSprites();
+  // animation(med, width * 0.85, height * 0.70);
+  // animation(tib, width * 0.25, height * 0.65);
 
   imageMode(CORNER);
   image(gamename, width * 0.12, height * 0.005, width * 0.50, height * 0.35);
